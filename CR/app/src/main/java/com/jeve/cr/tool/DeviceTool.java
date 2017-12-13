5dd649b0ee996811bea9be776ad2835181cff881
jevely <lijiawei_855@126.com>
2017-12-13 22:47:59 +0800

    代码上传
    设备信息获取
    长度单位转换

--- CR/app/src/main/java/com/jeve/cr/tool/DeviceTool.java
+++ CR/app/src/main/java/com/jeve/cr/tool/DeviceTool.java
@@ -2,7 +2,12 @@ package com.jeve.cr.tool;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Environment;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
@@ -46,4 +51,85 @@ public class DeviceTool {
         public int height;
     }
 
+    /**
+     * SD卡判断
+     */
+    public static boolean isSDCardAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    /**
+     * 是否有网
+     */
+    public static boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo mNetworkInfo = mConnectivityManager.getActiveNetworkInfo();
+            if (mNetworkInfo != null) {
+                return mNetworkInfo.isAvailable();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 返回版本名字
+     * 对应build.gradle中的versionName
+     */
+    public static String getVersionName(Context context) {
+        String versionName = "";
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionName = packInfo.versionName;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
+
+    /**
+     * 返回版本号
+     * 对应build.gradle中的versionCode
+     */
+    public static String getVersionCode(Context context) {
+        String versionCode = "";
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            versionCode = String.valueOf(packInfo.versionCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return versionCode;
+    }
+
+    /**
+     * 获取手机品牌
+     */
+    public static String getPhoneBrand() {
+        return android.os.Build.BRAND;
+    }
+
+    /**
+     * 获取手机型号
+     */
+    public static String getPhoneModel() {
+        return android.os.Build.MODEL;
+    }
+
+    /**
+     * 获取手机Android API等级（22、23 ...）
+     */
+    public static int getBuildLevel() {
+        return android.os.Build.VERSION.SDK_INT;
+    }
+
+    /**
+     * 获取手机Android 版本（4.4、5.0、5.1 ...）
+     */
+    public static String getBuildVersion() {
+        return android.os.Build.VERSION.RELEASE;
+    }
 }
---
+++ CR/app/src/main/java/com/jeve/cr/tool/UnitTransformTool.java
@@ -0,0 +1,29 @@
+package com.jeve.cr.tool;
+
+import android.content.Context;
+
+/**
+ * 单位转换工具类
+ * lijiawei
+ * 2017-12-13
+ */
+
+public class UnitTransformTool {
+
+    /**
+     * dp 转化为 px
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px 转化为 dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+}
