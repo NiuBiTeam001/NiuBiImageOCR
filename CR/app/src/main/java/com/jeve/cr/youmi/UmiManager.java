418f9d402e0b52df026eb02f09fc3951b95bec68
ZLdeSpirit <450303519@qq.com>
2018-01-02 16:59:55 +0800

    Umi
    Umi设置通知栏显示

--- CR/app/src/main/java/com/jeve/cr/youmi/UmiManager.java
+++ CR/app/src/main/java/com/jeve/cr/youmi/UmiManager.java
@@ -23,17 +23,31 @@ public class UmiManager {
     private static final String YOUMI_SECRET = "728753070452caa8";
 
     /**
-     *U mi 初始化
+     * U mi 初始化
      */
-    public static void initUmi(){
+    public static void initUmi() {
         //TODO 有米需要加小米应用渠道
-        AdManager.getInstance(CrApplication.getContext()).init(YOUMI_APP_ID, YOUMI_SECRET, true );//TODO 有米审核时也需要设置为true 发布的时候设置为false
+        AdManager.getInstance(CrApplication.getContext()).init(YOUMI_APP_ID, YOUMI_SECRET, true);//TODO 有米审核时也需要设置为true 发布的时候设置为false
+
+        // 设置是否在通知栏显示下载相关提示。默认为true，标识开启；设置为false则关闭。
+        AdManager.getInstance(CrApplication.getContext()).setIsDownloadTipsDisplayOnNotification(true);
+
+        // 获取是否在通知栏显示下载进度的值。
+        AdManager.getInstance(CrApplication.getContext()).isDownloadTipsDisplayOnNotification();
+
+
+        // 设置安装完成后是否在通知栏显示已安装成功的通知。默认为true，标识开启；设置为false则关闭。
+        AdManager.getInstance(CrApplication.getContext()).setIsInstallationSuccessTipsDisplayOnNotification(
+                true);
+
+        // 获取安装完成后是否在通知栏显示已安装成功的通知的值。
+        AdManager.getInstance(CrApplication.getContext()).isInstallationSuccessTipsDisplayOnNotification();
     }
 
     /**
      * 插屏广告，只需在启动应用的时候调用
      */
-    public static void initSpotAd(){
+    public static void initSpotAd() {
         SpotManager manager = SpotManager.getInstance(CrApplication.getContext());
         //设置横屏模式（当广告有横屏时，会使用横屏模式，默认为竖屏模式）
         manager.setImageType(SpotManager.IMAGE_TYPE_HORIZONTAL);
@@ -42,12 +56,12 @@ public class UmiManager {
         manager.requestSpot(new SpotRequestListener() {
             @Override
             public void onRequestSuccess() {
-                Log.d(TAG,"initSpotAd()----success");
+                Log.d(TAG, "initSpotAd()----success");
             }
 
             @Override
             public void onRequestFailed(int i) {
-                Log.d(TAG,"initSpotAd()----fail");
+                Log.d(TAG, "initSpotAd()----fail");
             }
         });
     }
@@ -55,26 +69,26 @@ public class UmiManager {
     /**
      * 展示插屏广告
      */
-    public static void showSpot(Activity activity){
+    public static void showSpot(Activity activity) {
         SpotManager.getInstance(activity).showSpot(activity, new SpotListener() {
             @Override
             public void onShowSuccess() {
-                Log.d(TAG,"onShowSuccess()");
+                Log.d(TAG, "onShowSuccess()");
             }
 
             @Override
             public void onShowFailed(int i) {
-                Log.d(TAG,"onShowFailed()");
+                Log.d(TAG, "onShowFailed()");
             }
 
             @Override
             public void onSpotClosed() {
-                Log.d(TAG,"onSpotClosed()");
+                Log.d(TAG, "onSpotClosed()");
             }
 
             @Override
             public void onSpotClicked(boolean b) {
-                Log.d(TAG,"onSpotClicked()");
+                Log.d(TAG, "onSpotClicked()");
             }
         });
     }
@@ -82,36 +96,37 @@ public class UmiManager {
     /**
      * 隐藏插屏广告
      */
-    public static void hideSpot(Activity activity){
+    public static void hideSpot(Activity activity) {
         SpotManager.getInstance(activity).hideSpot();
     }
 
     ////////////////////////////生命周期方法///////////////////////////////////////////////
+
     /**
      * ONPAUSE
      */
-    public static void spotOnPause(Activity activity){
+    public static void spotOnPause(Activity activity) {
         SpotManager.getInstance(activity).onPause();
     }
 
     /**
      * onStop
      */
-    public static void spotOnStop(Activity activity){
+    public static void spotOnStop(Activity activity) {
         SpotManager.getInstance(activity).onStop();
     }
 
     /**
      * onDestroy
      */
-    public static void spotOnDestroy(Activity activity){
+    public static void spotOnDestroy(Activity activity) {
         SpotManager.getInstance(activity).onDestroy();
     }
 
     /**
      * exit
      */
-    public static void spotExit(Activity activity){
+    public static void spotExit(Activity activity) {
         SpotManager.getInstance(activity).onAppExit();
     }
 }
